0aa6481e38078f92ce0660f445d33f45179a6f7d
Igor de Castro <igordecastro07@gmail.com>
2023-01-29 14:34:27 -0300

    fix: add route to update

--- src/routers/charactersRouter.ts
+++ src/routers/charactersRouter.ts
@@ -1,7 +1,7 @@
 import { validateModel } from "../middlewares/ModelValidation";
 import CharactersModel from "../models/charactersModel";
 import { Router } from "express";
-import { listAllCharacters, postCharacter, deleteCharacter } from "../controllers";
+import { listAllCharacters, postCharacter, deleteCharacter, updateCharacter } from "../controllers";
 
 const charactersRouter = Router();
 
@@ -9,6 +9,7 @@ charactersRouter
   .get("/characters", listAllCharacters)
   .post("/characters", validateModel(CharactersModel), postCharacter)
   .delete("/characters/:id", deleteCharacter)
+  .put("/characters/:id", updateCharacter)
 
 export {
     charactersRouter
